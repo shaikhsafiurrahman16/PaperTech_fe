d14c17dca16c7a92b65f3e9c815896550bb7bdcb
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2026-05-14 14:29:55 +0500

    add .env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || parseInt(env.PORT) || 3000,
+    },
+    preview: {
+      port: parseInt(env.VITE_PREVIEW_PORT) || 4173,
+    },
+  });
+};
