427dab3e9636f469d5b63e10b5679879a26600b0
shaikhsafiurrahman16 <shaikhsafiurrahman16@gmail.com>
2026-07-27 12:15:48 +0500

    Add Fixes

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,9 +3,7 @@ import store from '../stores';
 import { logout } from '../stores/authSlice';
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-  baseURL: 'http://tradestackbe.netlify.app/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 
   headers: {
     'Content-Type': 'application/json',
